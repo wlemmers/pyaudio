cf2f63787780f77915ac0a14371737bf53f28d4f
Wim Lemmers <lemmers.wim@gmail.com>
2021-11-27 16:27:05 +0100

    find loopback

--- list.py
+++ list.py
@@ -5,4 +5,7 @@ info = p.get_host_api_info_by_index(0)
 numdevices = info.get('deviceCount')
 for i in range(0, numdevices):
     if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+        name = p.get_device_info_by_host_api_device_index(0, i).get('name')
+        print("Input Device id ", i, " - ", name)
+        if "Loopback" in name:
+            print("%d"%i)
